8bdde8d01787fcbc64d94ec318803f396db48613
Diwakar Kumar Patel <diwakarpatel1011@gmail.com>
2025-02-28 15:52:48 +0530

    first commit

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import {registerUser,loginUser, userCredit, paymentRazorpay,verifyRazorpay} from '../controllers/userController.js'
+import userAuth from '../middlewares/auth.js'
+
+const userRouter = express.Router()
+
+userRouter.post('/register',registerUser)
+userRouter.post('/login',loginUser)
+userRouter.get('/credits',userAuth,userCredit)
+userRouter.post('/pay-razor',userAuth,paymentRazorpay)
+userRouter.post('/verify-razor',verifyRazorpay)
+
+export default userRouter
+
+//http://localhost:4000/api/user/register
