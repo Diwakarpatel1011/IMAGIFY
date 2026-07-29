5a729372c7261d59d6a06c680ad3a3ab1c70e830
Diwakar Kumar Patel <diwakarpatel1011@gmail.com>
2026-07-29 16:45:38 +0530

    new link

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://imagify-frontend-tau.vercel.app"
+        "https://imagify-diwakar-kumar-patels-projects.vercel.app/"
     ],
     credentials: true
 }))
